d22ca859b1444fc370ad72cc1104511dd41784a9
k5va <k5va@yandex.ru>
2024-11-23 11:43:42 +0100

    Optimizes code

--- gateway/src/main/java/org/k5va/config/SecurityConfig.java
+++ gateway/src/main/java/org/k5va/config/SecurityConfig.java
@@ -3,12 +3,10 @@ package org.k5va.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.ReactiveAuthenticationManagerResolver;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtIssuerReactiveAuthenticationManagerResolver;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.web.server.ServerWebExchange;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -23,12 +21,11 @@ public class SecurityConfig {
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(auth -> auth.anyExchange().authenticated())
                 .oauth2ResourceServer(customizer ->
-                        customizer.authenticationManagerResolver(resolver()))
+                        customizer.authenticationManagerResolver(
+                                JwtIssuerReactiveAuthenticationManagerResolver
+                                        .fromTrustedIssuers(issuerUri)
+                        )
+                )
                 .build();
-    }
-
-    @Bean
-    ReactiveAuthenticationManagerResolver<ServerWebExchange> resolver() {
-        return JwtIssuerReactiveAuthenticationManagerResolver.fromTrustedIssuers(issuerUri);
     }
 }
