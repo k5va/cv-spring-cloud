2c88b7a5dca84e171ac612edb3085c551ad74354
k5va <k5va@yandex.ru>
2024-11-23 11:37:05 +0100

    Refactors gateway to use JwtIssuerReactiveAuthenticationManagerResolver

--- gateway/src/main/java/org/k5va/config/SecurityConfig.java
+++ gateway/src/main/java/org/k5va/config/SecurityConfig.java
@@ -3,14 +3,12 @@ package org.k5va.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.ReactiveAuthenticationManagerResolver;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
-import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
-import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
-import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtIssuerReactiveAuthenticationManagerResolver;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import reactor.core.publisher.Flux;
+import org.springframework.web.server.ServerWebExchange;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -24,23 +22,13 @@ public class SecurityConfig {
         return http
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(auth -> auth.anyExchange().authenticated())
-                .oauth2ResourceServer(oAuth2 -> oAuth2.jwt(
-                        jwtSpec -> jwtSpec.jwtAuthenticationConverter(jwtAuthenticationConverter())))
+                .oauth2ResourceServer(customizer ->
+                        customizer.authenticationManagerResolver(resolver()))
                 .build();
     }
 
     @Bean
-    public ReactiveJwtAuthenticationConverter jwtAuthenticationConverter() {
-        JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
-        ReactiveJwtAuthenticationConverter jwtAuthenticationConverter = new ReactiveJwtAuthenticationConverter();
-        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(
-                jwt -> Flux.fromIterable(jwtGrantedAuthoritiesConverter.convert(jwt)));
-
-        return jwtAuthenticationConverter;
-    }
-
-    @Bean
-    public ReactiveJwtDecoder jwtDecoder() {
-        return NimbusReactiveJwtDecoder.withJwkSetUri(issuerUri).build();
+    ReactiveAuthenticationManagerResolver<ServerWebExchange> resolver() {
+        return JwtIssuerReactiveAuthenticationManagerResolver.fromTrustedIssuers(issuerUri);
     }
 }
