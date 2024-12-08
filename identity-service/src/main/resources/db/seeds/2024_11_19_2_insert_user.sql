365fa61e5c95e4e97491cf127183ec71c0c0326d
k5va <k5va@yandex.ru>
2024-12-08 11:28:07 +0100

    Fixes seed test user password

--- identity-service/src/main/resources/db/seeds/2024_11_19_2_insert_user.sql
+++ identity-service/src/main/resources/db/seeds/2024_11_19_2_insert_user.sql
@@ -1,7 +1,7 @@
 --liquibase formatted sql
 --changeset k5va:insert-default-user splitStatements:true endDelimiter:;
 INSERT INTO users (id, name, email, password)
-VALUES ('f7f5f79e-521b-46ed-8af8-2a9491c56a1f', 'John Doe', 'john@mail.com', '$2a$10$8408DFYmFgfb3055bJ4GmO.k3DJEGe04W2/TeAQcBkAY1UwNhYDde');
+VALUES ('f7f5f79e-521b-46ed-8af8-2a9491c56a1f', 'John Doe', 'john@mail.com', '{bcrypt}$2a$10$8408DFYmFgfb3055bJ4GmO.k3DJEGe04W2/TeAQcBkAY1UwNhYDde');
 INSERT INTO user_roles (user_id, role_id)
 VALUES ('f7f5f79e-521b-46ed-8af8-2a9491c56a1f', '84a9bffd-a390-487c-92a4-97dff0d10864');
 INSERT INTO user_roles (user_id, role_id)
